9b73b03932be7dbbd82c8cbe417d2696f75e582b
Erick Hoogstrate <e.hoogstrate@student.tue.nl>
2023-05-23 12:41:36 +0200

    Update for loading training data

--- data.py
+++ data.py
@@ -3,25 +3,20 @@ import torch
 import pandas as pd
 
 TRAIN_DATA = r"disc-benchmark-files\training-data.csv"
-TEST_DATA = r"disc-benchmark-files\test-simulation-submission-file.csv"
 
 
-def load_data(train_test="train", as_tensor=True):
+def load_data(as_tensor=True):
     """
-    Loads training or test data from given files.
+    Loads training data from given files.
 
     params:
-    train_test: whether to load train or test data
     as_tensor: whether to output data as a torch tensor or not
 
     returns:
     x_data, y_data
     """
 
-    if train_test == "train":
-        data = pd.read_csv(TRAIN_DATA).to_numpy()
-    else:
-        data = pd.read_csv(TEST_DATA).to_numpy()
+    data = pd.read_csv(TRAIN_DATA).to_numpy()
 
     if as_tensor:
         data = torch.tensor(data)
