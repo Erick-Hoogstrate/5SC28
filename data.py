e90450d77fdfcc23a8683affb20abc4a2b3eeca3
Erick Hoogstrate <e.hoogstrate@student.tue.nl>
2023-05-26 16:09:44 +0200

    NARX GP

--- data.py
+++ data.py
@@ -1,48 +1,61 @@
 import numpy as np
+from sklearn.model_selection import train_test_split
 import torch
 import pandas as pd
 
 TRAIN_DATA = r"disc-benchmark-files\training-data.csv"
-TEST_DATA = r"disc-benchmark-files\test-simulation-submission-file.csv"
 
 
-def load_data(train_test="train", as_tensor=True):
+def load_data(section="train", split=[0.6, 0.2, 0.2], as_tensor=True):
     """
-    Loads training or test data from given files.
+    Loads training, validation or test data from given files.
 
     params:
-    train_test: whether to load train or test data
+    section: whether to load train, validation or test data
+    split: training, validation, test split. "[train, val, test]"
     as_tensor: whether to output data as a torch tensor or not
 
     returns:
     x_data, y_data
     """
+    data = pd.read_csv(TRAIN_DATA).to_numpy()
+    X_train_val, X_test, y_train_val, y_test = train_test_split(data[:, 0], data[:, 1], test_size=split[2], shuffle=False)
+    X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, test_size=split[1] / (split[0] + split[1]), shuffle=False)
 
-    if train_test == "train":
-        data = pd.read_csv(TRAIN_DATA).to_numpy()
+    if section == "train":
+        x = X_train
+        y = y_train
+    elif section == "validation":
+        x = X_val
+        y = y_val
+    elif section == "test":
+        x = X_test
+        y = y_test
     else:
-        data = pd.read_csv(TEST_DATA).to_numpy()
+        raise ValueError("Invalid section value. Must be 'train', 'validation', or 'test'.")
 
     if as_tensor:
-        data = torch.tensor(data)
+        x = torch.tensor(x)
+        y = torch.tensor(y)
 
-    return data[:, 0], data[:, 1]
+    return x, y
 
 
-def load_narx_data(n_a, n_b, train_test="train", as_tensor=True):
+def load_narx_data(n_a, n_b, section="train", split=[0.6, 0.2, 0.2], as_tensor=True):
     """
-    Loads training or test data from given files, and formats it as a NARX input([X_{k-n_b}, ..., X_{k-1}, Y_{k-n_a}, ... , Y_{k-1}]).
+    Loads training, validation or test data from given files, and formats it as a NARX input([X_{k-n_b}, ..., X_{k-1}, Y_{k-n_a}, ... , Y_{k-1}]).
 
     params:
     n_a: amount of X-samples considered
     n_b: amount of Y-samples considered
-    train_test: whether to load train or test data
+    section: whether to load train, validation or test data
+    split: training, validation, test split. "[train, val, test]"
     as_tensor: whether to output data as a torch tensor or not
 
     returns:
     x_data, y_data
     """
-    x, y = load_data(train_test, as_tensor)
+    x, y = load_data(section, split, as_tensor)
 
     X = []
     Y = []
