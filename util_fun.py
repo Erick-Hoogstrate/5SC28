2ba6b537c093a8d754fb341f135fa3b747deab78
LeonardoStrik <61874207+LeonardoStrik@users.noreply.github.com>
2023-05-30 23:00:49 +0200

    add support for heterogeneous input types in nrms calculation

--- util_fun.py
+++ util_fun.py
@@ -2,11 +2,10 @@ import torch
 import numpy as np
 
 
-def calculate_error_nrms(y_predicted:torch.Tensor|np.ndarray, y_true:torch.Tensor|np.ndarray) -> float:
-    if type(y_predicted)==torch.Tensor:
-        nrms = torch.mean((y_predicted - y_true) ** 2).item() ** 0.5 /torch.std(y_true)
-        nrms=nrms.item()
-    else:
-        nrms = np.mean((y_predicted - y_true) ** 2).item() ** 0.5 /np.std(y_true)
+def calculate_error_nrms(
+    y_predicted: torch.Tensor | np.ndarray, y_true: torch.Tensor | np.ndarray
+) -> float:
+    y_predicted, y_true = [y.detach().cpu().numpy() if type(y) == torch.Tensor else y for y in [y_predicted, y_true]]
+    nrms = np.mean((y_predicted - y_true) ** 2) ** 0.5 / np.std(y_true)
 
     return nrms
