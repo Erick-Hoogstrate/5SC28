b3281fc744dfb1c5b646bb87ee03517596f08eab
Erick Hoogstrate <e.hoogstrate@student.tue.nl>
2023-06-05 13:42:42 +0200

    Convert NRMS to percentage

--- util_fun.py
+++ util_fun.py
@@ -2,8 +2,8 @@ import torch
 import numpy as np
 
 
-def calculate_error_nrms(y_predicted: torch.Tensor or np.ndarray, y_true: torch.Tensor or np.ndarray) -> float:
+def calculate_error_nrms(y_predicted: torch.Tensor or np.ndarray, y_true: torch.Tensor or np.ndarray, as_percentage: bool=True) -> float:
     y_predicted, y_true = [y.detach().cpu().numpy() if type(y) == torch.Tensor else y for y in [y_predicted, y_true]]
     nrms = np.mean((y_predicted - y_true) ** 2) ** 0.5 / np.std(y_true)
 
-    return nrms
+    return nrms*100 if as_percentage else nrms
