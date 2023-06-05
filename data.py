21fa7dbd33a817e0147e35183210c7bedd6f8f7e
LeonardoStrik <61874207+LeonardoStrik@users.noreply.github.com>
2023-06-05 10:09:54 +0200

    adjust data loading

--- data.py
+++ data.py
@@ -1,27 +1,22 @@
 import numpy as np
 import torch
 import pandas as pd
+from typing import Union
 
 TRAIN_DATA = r"disc-benchmark-files\training-data.csv"
-TEST_DATA = r"disc-benchmark-files\test-simulation-submission-file.csv"
 
-
-def load_data(train_test="train", as_tensor=True):
+def load_data( as_tensor:bool=True)->Union[pd.DataFrame, torch.Tensor]:
     """
     Loads training or test data from given files.
 
     params:
-    train_test: whether to load train or test data
     as_tensor: whether to output data as a torch tensor or not
 
     returns:
     x_data, y_data
     """
 
-    if train_test == "train":
-        data = pd.read_csv(TRAIN_DATA).to_numpy()
-    else:
-        data = pd.read_csv(TEST_DATA).to_numpy()
+    data = pd.read_csv(TRAIN_DATA).to_numpy()
 
     if as_tensor:
         data = torch.tensor(data)
