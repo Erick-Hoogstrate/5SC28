cb594ce4d029110b4a82e23520552d31af829cf8
Erick Hoogstrate <e.hoogstrate@student.tue.nl>
2023-06-22 15:21:58 +0200

    Test

--- settings_pendulum.m
+++ settings_pendulum.m
@@ -59,15 +59,15 @@ poli = [1 3 4];             % variables that serve as inputs to the policy
 difi = [1 2];               % variables that are learned via differences
 
 % 2. Set up the scenario
-dt = 0.1;                      % [s] sampling time
+dt = 0.025;                      % [s] sampling time
 T = 4;                         % [s] prediction time
 H = ceil(T/dt);                % prediction steps (optimization horizon)
 mu0 = [0 0]';                  % initial state mean
 S0 = 0.01*eye(2);              % initial state variance
-N = 50;                        % number of policy optimizations
-J = 5;                         % no. of inital training rollouts (of length H)
+N = 10;                        % number of policy optimizations
+J = 1;                         % no. of inital training rollouts (of length H)
 K = 1;                         % number of initial states for which we optimize
-nc = 20;                       % size of controller training set
+nc = 10;                       % size of controller training set
 
 % 3. Set up the plant structure
 plant.dynamics = @dynamics_pendulum;    % dynamics ODE function
