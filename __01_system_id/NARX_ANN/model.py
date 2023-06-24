b8815c3774aa5b209943b0f873a1efe28a2d2fe7
LeonardoStrik <61874207+LeonardoStrik@users.noreply.github.com>
2023-06-24 12:08:35 +0200

    add submission files

--- __01_system_id/NARX_ANN/model.py
+++ __01_system_id/NARX_ANN/model.py
@@ -13,10 +13,10 @@ class Narx(nn.Module):
         super().__init__()
 
         # add input, hidden, and output layers
-        layers = [nn.Linear(n_in, n_hidden_nodes), nn.LeakyReLU()]
+        layers = [nn.Linear(n_in, n_hidden_nodes), nn.Sigmoid()]
         for _ in range(n_hidden_layers):
             layers.append(nn.Linear(n_hidden_nodes, n_hidden_nodes))
-            layers.append(nn.LeakyReLU())
+            layers.append(nn.ReLU())
         layers += [nn.Linear(n_hidden_nodes, 1)]
         layers = [layer.double() for layer in layers]
 
